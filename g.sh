9ec2bfb3673893fb72a28430e6c9a93d292e1667
Saska <saska.donges@aacglobal.com>
2017-03-02 14:10:25 +0200

    huoh

---
+++ g.sh
@@ -0,0 +1,13 @@
+﻿
+git clone https://github.com/jjanii/MazeSolver
+git clone https://github.com/aapzu/super-duper-pentago
+git clone https://github.com/pqkallio/DASP-5000
+git clone https://github.com/mortsi/multi-pack
+git clone https://github.com/emlai/emregex
+git clone https://github.com/inla/Sokkelo
+git clone https://github.com/anttilip/pathfinding-visualizer
+git clone https://github.com/Shyles/RegExWizard
+git clone https://github.com/KaroliinaM/shakki
+git clone https://github.com/RIR/Huffman
+git clone https://github.com/praetoria/dns_tunnel
+git clone https://github.com/rivorivo/TiraLab17
